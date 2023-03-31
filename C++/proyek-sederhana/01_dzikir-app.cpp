e03725d960f646c9d92298de91e76f2f7caa42df
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-01 01:34:21 +0700

    refactoring!

--- C++/proyek-sederhana/01_dzikir-app.cpp
+++ C++/proyek-sederhana/01_dzikir-app.cpp
@@ -2,12 +2,12 @@
  اللَّهُمَّ رَبَّ النَّاسِ أَذْهِبِ الْبَأْسَ اشْفِ أَنْتَ الشَّافِي لَا شَافِيَ إلَّا أَنْتَ شِفَاءً لَا يُغَادِرُ سَقْمًا
  Luhud trisnyawati bin fulan.
 */
+
 #include <iostream>
 #include <string>
-#include <iomanip>
 
-void headerProgram(){ 
-std::cout<<R"(
+void headerProgram() {
+    std::cout << R"(
 ██████╗░███████╗██╗██╗░░██╗██╗██████╗░
 ██╔══██╗╚════██║██║██║░██╔╝██║██╔══██╗
 ██║░░██║░░███╔═╝██║█████═╝░██║██████╔╝
@@ -15,45 +15,40 @@ std::cout<<R"(
 ██████╔╝███████╗██║██║░╚██╗██║██║░░██║
 ╚═════╝░╚══════╝╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚═╝
 -------------------------------------
-)"<<'\n';
+)" << '\n';
 }
 
-std::string checkValiInput(){
+std::string getInput() {
+  std::string input;
+  std::cout << "Masukan kalimat Dzikir\t: ";
+  std::cin >> input;
+  return input;
+}
+
+int getNumForLooping() {
   std::string numForLooping;
-  std::cout<<'\n'<<"Ingin berapa Dzikir hari ini\t: ";std::cin>>numForLooping;
-  for (int i=0; i<numForLooping.length(); i++){
-    if ((numForLooping[i]>='a' && numForLooping[i]<='z')
-        || (numForLooping[i]>='A' && numForLooping[i]<='Z')) {
-      std::cout<<"Input harus berupa Angka!"<<'\n';
-      return checkValiInput();
+  std::cout << '\n' << "Ingin berapa Dzikir hari ini\t: ";std::cin >> numForLooping;
+  int finalOutput = 0;
+    for (int i = 0; i < numForLooping.length(); i++) {
+      if ((numForLooping[i] >= 'a' && numForLooping[i] <= 'z') || (numForLooping[i] >= 'A' && numForLooping[i] <= 'Z')) {
+        std::cout << "Input harus berupa Angka!" << '\n';
+        return getNumForLooping();
+      }
+      finalOutput = finalOutput * 10 + numForLooping[i] - '0';
     }
-  }
-  return numForLooping;
+  return finalOutput;
 }
 
-int stringToInteger(const std::string& str) {
-  int tempConverter = 0;
-  int valueChar;
-  for (int i=0; i<str.length(); i++) {
-    tempConverter *= 10;
-    valueChar = str[i] - '0';
-    tempConverter = (tempConverter + valueChar);
-  }
-  return tempConverter;
-}
-
-int main(int argc, char *argv[]){  
+int main(int argc, char *argv[]) {
   headerProgram();
   char u;
-  std::string input;
-  do{
-    std::string ResultValidation = checkValiInput();
-    int finalOutput = stringToInteger(ResultValidation);
-      for(int i=0; i<finalOutput; i++){
-        std::cout<<(i+1)<<". Masukan kalimat Dzikir\t: ";std::cin>>input;
-      }
-    std::cout<<"Dzikir selesai dengan kalimat "<<input<<" dengan total sejumlah "<<finalOutput<<'\n';
-    std::cout<<"Apakah anda ingin mengulang kembali [Y/n]: ";std::cin>> u;
-  }while(u=='y' || u=='Y');
+    do {
+      int finalOutput = getNumForLooping();
+        for (int i = 0; i < finalOutput; i++) {
+          std::string input = getInput();
+        }
+        std::cout << "Dzikir selesai dengan total sejumlah " << finalOutput << '\n';
+        std::cout << "Apakah anda ingin mengulang kembali [Y/n]: ";std::cin >> u;
+    } while (u == 'y' || u == 'Y');
   return 0;
 }
