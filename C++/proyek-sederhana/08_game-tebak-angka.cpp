df55198a60f46ae23eb04be9bf5ea210dc119d0b
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-01 05:28:54 +0700

    ::(

--- C++/proyek-sederhana/08_game-tebak-angka.cpp
+++ C++/proyek-sederhana/08_game-tebak-angka.cpp
@@ -10,9 +10,9 @@ void playGame(int randomNumber){
     while(true){
       std::cout<<"Masukan tebakan Anda: ";std::cin >> playerGuess;
       guess_count++;
-        if(player_guess > randomNumber) {
+        if(playerGuess > randomNumber) {
           std::cout<<"Angka terlalu tinggi, silahkan coba lagi."<<'\n';
-        } else if(player_guess < randomNumber) {
+        } else if(playerGuess < randomNumber) {
           std::cout<<"Angka terlalu rendah, silahkan coba lagi."<<'\n';
         } else {
           std::cout<<"Selamat, Anda benar dengan"<<guessCount << " percobaan!"<<'\n';
