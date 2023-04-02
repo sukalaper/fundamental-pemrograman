28dc82617c7e6b22ba2c1996e3b1dc280a859b12
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-02 16:45:17 +0700

    Update 07_random-password-generator.cpp

--- C++/proyek-sederhana/07_random-password-generator.cpp
+++ C++/proyek-sederhana/07_random-password-generator.cpp
@@ -3,26 +3,25 @@
 
 const std::string ALNUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$_&+()!?*";
 
-int getRandomIndex(int maxIndex) {
+int getRandomIndex(int maxIndex){
   return rand() % maxIndex;
 }
 
-int getLengthFromUser() {
+int getLengthFromUser(){
   int length;
-  std::cout << "Masukan angka: ";
-  std::cin >> length;
+  std::cout << "Masukan angka: ";std::cin >> length;
   return length;
 }
 
-std::string generateRandomString(int length) {
+std::string generateRandomString(int length){
   std::string randomStr;
-  for (int i = 0; i < length; ++i) {
+  for (int i = 0; i < length; ++i){
     randomStr += ALNUM[getRandomIndex(ALNUM.size())];
   }
   return randomStr;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
   srand(time(0));
   int length = getLengthFromUser();
   std::string randomString = generateRandomString(length);
