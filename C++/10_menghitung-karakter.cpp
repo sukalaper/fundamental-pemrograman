ee5f7da0bdcbc0b3ba571a614289ed90a313ce2e
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-09 03:07:55 +0700

    Create 10_menghitung-karakter.cpp

---
+++ C++/10_menghitung-karakter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+class inputReader{
+public:
+  std::string readInput(){
+    std::string input;
+    std::cout << "Masukkan input Anda: ";std::getline(std::cin, input);
+    return input;
+  }
+};
+
+class characterCounter{
+public:
+  int countCharacters(const std::string& input){
+    return input.length();
+  }
+};
+
+// Modul untuk menampilkan hasil
+class outputPrinter {
+public:
+  void printResult(int count) {
+    std::cout<<"Jumlah karakter pada input Anda adalah: "<<count<<'\n';
+  }
+};
+
+// Program utama
+int main(int argc, char *argv[]){
+  inputReader reader;
+  characterCounter counter;
+  outputPrinter printer;
+
+  std::string input = reader.readInput();
+  int count = counter.countCharacters(input);
+  printer.printResult(count);
+
+  return 0;
+}
