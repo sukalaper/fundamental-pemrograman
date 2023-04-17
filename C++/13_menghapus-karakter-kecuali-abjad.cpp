f1f357c6f9d053c47c8c8e09e855e8bec20ba4f3
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-18 02:48:45 +0700

    Create 13_menghapus-karakter-kecuali-abjad.cpp

---
+++ C++/13_menghapus-karakter-kecuali-abjad.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+class StringManipulator{
+public:
+  std::string removeNonAlphabeticChars(std::string str){
+    std::string result = "";
+      for (char c : str){
+        if (isalpha(c)){
+          result += c;
+        }
+      }
+  return result;
+  }
+};
+
+class UserInput{
+public:
+  std::string getString(){
+    std::string input;
+    std::cout<<"Masukan Kalimat: ";std::getline(std::cin, input);
+    return input;
+  }
+};
+
+int main(){
+    UserInput input;
+    StringManipulator manipulator;
+    std::string str = input.getString();
+    std::string result = manipulator.removeNonAlphabeticChars(str);
+    std::cout<<"Hasil: "<<result<<'\n';
+    return 0;
+}
