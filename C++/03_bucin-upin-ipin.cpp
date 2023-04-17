cf19480ec84e0f0e585a8c4191e43cbd1b645978
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-18 02:31:18 +0700

    Update 03_bucin-upin-ipin.cpp

--- C++/03_bucin-upin-ipin.cpp
+++ C++/03_bucin-upin-ipin.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <string>
 
-std::string removeCharAtOne(const std::string& str) {
+std::string removeCharAtOne(const std::string& str){
   std::string newStr(str);
   newStr.erase(1);
   return newStr;
 }
 
-void printString(const std::string& str) {
+void printString(const std::string& str){
   std::cout<<str<<'\n';
 }
 
-std::string cartoonOne() {
+std::string cartoonOne(){
   std::string str("Upin");
   std::string newStr = removeCharAtOne(str);
   printString(newStr);
   return newStr;
 }
 
-std::string cartoonTwo() {
+std::string cartoonTwo(){
   std::string str("Ipin");
   std::string newStr = removeCharAtOne(str);
   printString(newStr);
