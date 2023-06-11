a6363beb57207027b9a61daa321eb8c6dc6042d4
Nggi <anggirrr31@gmail.com>
2023-06-12 02:23:01 +0700

    Update script.js

--- Fullstack/Landing-page/CV-page/script.js
+++ Fullstack/Landing-page/CV-page/script.js
@@ -13,6 +13,10 @@ document.getElementById('cvForm').addEventListener('submit', function(event) {
   var pendidikanKedua = document.getElementById('pendidikan_2').value;
   var tahunPendidikanKedua = document.getElementById('tahun_2').value;
   var keteranganPendidikanKedua = document.getElementById('keterangan_pendidikan_2').value;
+  var tempatKerja = document.getElementById('tempat_kerja').value;
+  var posisiKerja = document.getElementById('posisi_kerja').value;
+  var tahunKerja = document.getElementById('tahun_kerja').value;
+  var keteranganKerja = document.getElementById('keterangan_kerja').value;
   var cvData = {
     nama: nama,
     posisi: posisi,
@@ -27,7 +31,10 @@ document.getElementById('cvForm').addEventListener('submit', function(event) {
     pendidikan_2: pendidikanKedua,
     tahun_2: tahunPendidikanKedua,
     keterangan_pendidikan_2: keteranganPendidikanKedua,
-    pengalaman: pengalamanData
+    tempat_kerja: tempatKerja,
+    posisi_kerja: posisiKerja,
+    tahun_kerja: tahunKerja,
+    keterangan_kerja: keteranganKerja,
   };
   var cvHTML = generateCV(cvData);
   document.getElementById('hasilCV').innerHTML = cvHTML;
@@ -37,10 +44,12 @@ function generateCV(data) {
     <div style="line-height: 10px;">
       <h2 style="color: #213555; text-transform: uppercase;">${data.nama}</h2>
       <h3 style="color: #213555;">${data.posisi}</h3>
-      <i class="fas fa-home"></i> ${data.alamat} <i class="fab fa-whatsapp" style="padding-left: 3px;"></i> ${data.telp} <i class="fa fa-envelope" style="padding-left: 3px;"></i> ${data.email} ${data.social ? `<i class="fas fa-link" style="padding-left: 3px;"></i> ${data.social}` : ''} 
+      <div style="margin-top: 10px;">
+        <i class="fas fa-home"></i> ${data.alamat} <i class="fab fa-whatsapp" style="padding-left: 3px;"></i> ${data.telp} <i class="fa fa-envelope" style="padding-left: 3px;"></i> ${data.email} ${data.social ? `<i class="fas fa-link" style="padding-left: 3px;"></i> ${data.social}` : ''}
+      </div>
     </div>
     <h3 style="margin-top: 35px; padding: 6px; letter-spacing: 3px; color: #FBFBFB; background-color: #213555;">RINGKASAN</h3>
-    <p>${data.ringkasan}</p>
+    <div>${data.ringkasan}</div>
     <h3 style="margin-top: 35px; padding: 6px; letter-spacing: 3px; color: #FBFBFB; background-color: #213555;">PENDIDIKAN</h3>
     <div style="text-transform: uppercase; font-weight: bold;">
       ${data.pendidikan} | ${data.tahun}
@@ -51,6 +60,15 @@ function generateCV(data) {
     </div>
     <div>${data.keterangan_pendidikan_2}</div>
     <h3 style="margin-top: 35px; padding: 6px; letter-spacing: 3px; color: #FBFBFB; background-color: #213555;">PENGALAMAN</h3>
+    <div style="text-transform: uppercase; font-weight: bold;">
+      ${data.tempat_kerja}
+    </div>
+    <div style="text-transform: uppercase; font-weight: bold;">
+      ${data.posisi_kerja} | ${data.tahun_kerja}
+    </div>
+    <div style="margin-top: 3px;">
+      ${data.keterangan_kerja}
+    </div>
   `;
   return cvHTML;
 }
