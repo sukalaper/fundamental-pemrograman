068ae7a9674b402f4bff37a7d6c19545a2fea9ee
Anggiramadyansyah <anggirrr31@gmail.com>
2023-06-29 00:16:50 +0700

    Update 07_random-password-generator.cpp

--- C++/07_random-password-generator.cpp
+++ C++/07_random-password-generator.cpp
@@ -3,8 +3,8 @@
 
 class UIManager {
 public:
-    static void printHeader() {
-        std::cout << R"(
+  static void printHeader() {
+    std::cout << R"(
 ______  ___  _____ _____ _    _ _________________   _____ _____ _   _ ___________  ___ _____ ___________ 
 | ___ \/ _ \/  ___/  ___| |  | |  _  | ___ |  _  \ |  __ |  ___| \ | |  ___| ___ \/ _ |_   _|  _  | ___ \
 | |_/ / /_\ \ `--.\ `--.| |  | | | | | |_/ | | | | | |  \| |__ |  \| | |__ | |_/ / /_\ \| | | | | | |_/ /
@@ -12,30 +12,30 @@ ______  ___  _____ _____ _    _ _________________   _____ _____ _   _ __________
 | |   | | | /\__/ /\__/ \  /\  \ \_/ | |\ \| |/ /  | |_\ | |___| |\  | |___| |\ \| | | || | \ \_/ | |\ \ 
 \_|   \_| |_\____/\____/ \/  \/ \___/\_| \_|___/    \____\____/\_| \_\____/\_| \_\_| |_/\_/  \___/\_| \_|                                                                              
 )" << '\n';
-    }
+  }
 };
 
 const std::string ALNUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$_&+()!?*/\\[]^{}";
 
-int getRandomIndex(int maxIndex) {
+int getRandomIndex(int maxIndex){
   return rand() % maxIndex;
 }
 
-int getLengthFromUser() {
+int getLengthFromUser(){
   std::string inputUser;
   int finalOutput = 0;
   std::cout<<"Masukan panjang password yang diinginkan: ";std::cin>>inputUser;
-    for (int a = 0; a < inputUser.length(); a++) {
-        if ((inputUser[a] >= 'a' && inputUser[a] <= 'z') || (inputUser[a] >= 'A' && inputUser[a] <= 'Z')) {
-            std::cout << "Input harus berupa angka, bukan abjad!" << '\n';
-            return getLengthFromUser();
-        }
-        finalOutput = finalOutput * 10 + inputUser[a] - '0';
+    for (int a = 0; a < inputUser.length(); a++){
+      if ((inputUser[a] >= 'a' && inputUser[a] <= 'z') || (inputUser[a] >= 'A' && inputUser[a] <= 'Z')){
+        std::cout<<"Input harus berupa angka, bukan abjad!"<<'\n';
+          return getLengthFromUser();
+      }
+      finalOutput = finalOutput * 10 + inputUser[a] - '0';
     }
    return finalOutput;
 }
 
-int getNumStringFromUser() {
+int getNumStringFromUser(){
   int numStrings;  
   std::cout<<"Masukan banyaknya output password yang diinginkan: ";std::cin>>numStrings;
   return numStrings;
