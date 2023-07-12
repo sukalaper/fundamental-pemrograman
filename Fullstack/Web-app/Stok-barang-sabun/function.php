8afc0c22c4dc1d72f6adae2280c313cbcaece17f
Anggiramadyansyah <anggirrr31@gmail.com>
2023-07-13 03:53:42 +0700

    Update function.php
    Sangat berantakan...

--- Fullstack/Web-app/Stok-barang-sabun/function.php
+++ Fullstack/Web-app/Stok-barang-sabun/function.php
@@ -73,4 +73,72 @@ $conn = mysqli_connect("localhost","root","","stok-barang");
       header('location:index.php');
     }
   }
+// Mengubah Data Barang Masuk
+/*
+  if(isset($_POST['updatebarangmasuk'])){
+    $idbarang = $_POST['idbarang'];
+    $idmasuk = $_POST['idmasuk'];
+    $namabarang = $_POST['namabarang'];
+    $qty = $_POST['qty'];
+    $lihatstok = mysqli_query($conn,"SELECT * FROM stok WHERE idbarang='$idbarang'");
+    $stoknya = mysqli_fetch_array($lihatstok);
+    $stoksekarang = $stoknya['stok'];
+    $qtysekarang = mysqli_query($conn,"SELECT * FROM masuk WHERE idmasuk='$idmasuk'");
+    $qtynya = mysqli_fetch_array($qtysekarang);
+    $qtysekarang = $qtynya['qty'];
+    if($qty>$qtysekarang){
+      $selisih = $qty-$qtysekarang;
+      $kurangi = $stoksekarang - $selisih;
+      $kurangistok = mysqli_query($conn,"UPDATE stok SET stok='$kurangi' WHERE idbarang='$idbarang'");
+      $updatenya = mysqli_query($conn,"UPDATE masuk SET qty='$qty' WHERE idmasuk='$idmasuk'");
+      if($kurangistok&&updatenya){
+        header('location:barangmasuk.php');
+      } else {
+        header('location:barangmasuk.php');
+      }
+    } else {
+      $selisih = $qtysekarang-$qty;
+      $kurangi = $stoksekarang + $selisih;
+      $kurangistok = mysqli_query($conn,"UPDATE stok SET stok='$kurangi' WHERE idbarang='$idbarang'");
+      $updatenya = mysqli_query($conn,"UPDATE masuk SET qty='$qty' WHERE idmasuk='$idmasuk'");
+      if($kurangistok&&updatenya){
+        header('location:barangmasuk.php');
+      } else {
+        header('location:barangmasuk.php');
+      }
+    }
+  }
+*/
+if (isset($_POST['updatebarangmasuk'])) {
+  $idbarang = $_POST['idbarang'];
+  $idmasuk = $_POST['idmasuk'];
+  $qty = $_POST['qty'];
+
+  // Mendapatkan stok barang sekarang
+  $result_cek_stok_sekarang = mysqli_query($conn, "SELECT * FROM stok WHERE idbarang='$idbarang'");
+  $result_ambil_data = mysqli_fetch_array($result_cek_stok_sekarang);
+  $stoksekarang = $result_ambil_data['jumlahbarang'];
+
+  // Mendapatkan jumlah qty sekarang
+  $result_cek_qty_sekarang = mysqli_query($conn, "SELECT * FROM masuk WHERE idmasuk='$idmasuk'");
+  $result_ambil_qty = mysqli_fetch_array($result_cek_qty_sekarang);
+  $qtysekarang = $result_ambil_qty['qty'];
+
+  // Menghitung selisih qty baru dengan qty sekarang
+  $selisih = $qty - $qtysekarang;
+  
+  // Mengupdate stok barang
+  $stokbaru = $stoksekarang + $selisih;
+  $result_update_stok = mysqli_query($conn, "UPDATE stok SET jumlahbarang='$stokbaru' WHERE idbarang='$idbarang'");
+
+  // Mengupdate qty barang masuk
+  $result_update_qty = mysqli_query($conn, "UPDATE masuk SET qty='$qty' WHERE idmasuk='$idmasuk'");
+
+  if ($result_update_stok && $result_update_qty) {
+    header('location:barang-masuk.php');
+  } else {
+    echo 'Gagal mengubah data barang masuk!';
+    header('location:barang-masuk.php');
+  }
+}
 ?>
