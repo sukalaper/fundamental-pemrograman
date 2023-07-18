ec6d3ab65b9e278523900c31fccfddfb4751ddb9
Anggiramadyansyah <anggirrr31@gmail.com>
2023-07-19 01:33:45 +0700

    Update 01_dzikir-sederhana.cpp
    Refaktorisasi agar mematuhi SRP.

--- C++/01_dzikir-sederhana.cpp
+++ C++/01_dzikir-sederhana.cpp
@@ -1,13 +1,8 @@
-/*
- اللَّهُمَّ رَبَّ النَّاسِ أَذْهِبِ الْبَأْسَ اشْفِ أَنْتَ الشَّافِي لَا شَافِيَ إلَّا أَنْتَ شِفَاءً لَا يُغَادِرُ سَقْمًا
- Luhud trisnyawati bin fulan.
-*/
-
 #include <iostream>
 #include <string>
 
 void headerProgram(){
-    std::cout << R"(
+  std::cout<<R"(
 ██████╗░███████╗██╗██╗░░██╗██╗██████╗░
 ██╔══██╗╚════██║██║██║░██╔╝██║██╔══██╗
 ██║░░██║░░███╔═╝██║█████═╝░██║██████╔╝
@@ -28,26 +23,40 @@ int getNumForLooping(){
   std::string numForLooping;
   std::cout<<'\n'<<"Ingin berapa Dzikir hari ini\t: ";std::cin>>numForLooping;
   int finalOutput = 0;
-    for (int i = 0; i < numForLooping.length(); i++){
-      if ((numForLooping[i] >= 'a' && numForLooping[i] <= 'z') || (numForLooping[i] >= 'A' && numForLooping[i] <= 'Z')){
-        std::cout<<"Input harus berupa Angka!"<<'\n';
-        return getNumForLooping();
-      }
-      finalOutput = finalOutput * 10 + numForLooping[i] - '0';
+  for (int i = 0; i < numForLooping.length(); i++){
+    if ((numForLooping[i] >= 'a' && numForLooping[i] <= 'z') || (numForLooping[i] >= 'A' && numForLooping[i] <= 'Z')){
+      std::cout<<"Input harus berupa Angka!"<<'\n';
+      return getNumForLooping();
     }
+    finalOutput = finalOutput * 10 + numForLooping[i] - '0';
+  }
   return finalOutput;
 }
 
-int main(int argc, char *argv[]){
+void performDzikir(int numIterations){
+  for(int i = 0; i < numIterations; i++){
+    std::string input = getInput();
+  }
+}
+
+void displayResult(int numIterations){
+  std::cout<<"Dzikir selesai dengan total sejumlah "<<numIterations<<'\n';
+}
+
+bool askForRepeat(){
+  char answer;
+  std::cout<<"Apakah Anda ingin mengulang kembali [Y/n]: ";std::cin>>answer;
+  return (answer == 'y' || answer == 'Y');
+}
+
+int main(int argc, char* argv[]){
   headerProgram();
   char u;
-    do{
-      int finalOutput = getNumForLooping();
-        for (int i = 0; i < finalOutput; i++){
-          std::string input = getInput();
-        }
-        std::cout<<"Dzikir selesai dengan total sejumlah "<<finalOutput<<'\n';
-        std::cout<<"Apakah anda ingin mengulang kembali [Y/n]: ";std::cin>>u;
-    } while(u == 'y' || u == 'Y');
+  do{
+    int finalOutput = getNumForLooping();
+    performDzikir(finalOutput);
+    displayResult(finalOutput);
+    u = askForRepeat();
+  }while(u);
   return 0;
 }
