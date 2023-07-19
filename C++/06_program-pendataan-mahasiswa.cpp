01ee6d2f0a6e4911590cc757e9e69d11ecfb32d5
Anggiramadyansyah <anggirrr31@gmail.com>
2023-07-20 02:08:41 +0700

    Update 06_program-pendataan-mahasiswa.cpp
    Refaktorisasi agar mematuhi SRP.

--- C++/06_program-pendataan-mahasiswa.cpp
+++ C++/06_program-pendataan-mahasiswa.cpp
@@ -20,9 +20,9 @@ int cek_golongan(int penghasilan){
   int golongan = 0;
   if (penghasilan <= 2000000){
     golongan = 1;
-  } else if(penghasilan > 2000000 && penghasilan <= 3000000){
+  } else if (penghasilan > 2000000 && penghasilan <= 3000000){
     golongan = 2;
-  } else if(penghasilan > 3000000 && penghasilan <= 4000000){
+  } else if (penghasilan > 3000000 && penghasilan <= 4000000){
     golongan = 3;
   } else {
     golongan = 4;
@@ -45,7 +45,7 @@ void hitung_golongan_dan_ukt(std::vector<Mahasiswa>& daftar_mahasiswa){
     if (mhs.bidik_misi == 't' || mhs.bidik_misi == 'T'){
       mhs.golongan = cek_golongan(mhs.penghasilan_orang_tua);
       mhs.ukt = 900000 * mhs.golongan;
-    } else if(mhs.bidik_misi == 'y' || mhs.bidik_misi == 'Y'){
+    } else if (mhs.bidik_misi == 'y' || mhs.bidik_misi == 'Y'){
       mhs.ukt = 0;
       mhs.golongan = 0;
     }
@@ -58,22 +58,24 @@ void tampilkan_data(std::vector<Mahasiswa>& daftar_mahasiswa){
   std::cout<<"No. | Nama\t\t| NPM\t\t| Jurusan\t| Penghasilan Orang Tua\t| Golongan | UKT\n";
   std::cout<<"=============================================================================================\n";
   int no = 1;
-    for (Mahasiswa& mhs : daftar_mahasiswa) {
-      std::cout<<no++<<"\t| "<<mhs.nama<<"\t\t| "<<mhs.npm<<"\t\t| " <<mhs.jurusan<<"\t| " <<mhs.penghasilan_orang_tua<<"\t\t\t\t| "<<mhs.golongan<<"\t  | "<<mhs.ukt<< '\n';
-    }
-  std::cout<< "=============================================================================================\n";
+  for(Mahasiswa& mhs : daftar_mahasiswa){
+    std::cout<<no++<<"\t| "<<mhs.nama<<"\t\t| "<<mhs.npm<<"\t\t| "<<mhs.jurusan<<"\t| "
+              <<mhs.penghasilan_orang_tua<<"\t\t\t\t| "<<mhs.golongan<<"\t  | "<<mhs.ukt<<'\n';
+  }
+  std::cout<<"=============================================================================================\n";
 }
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]){
   header_program();
   std::vector<Mahasiswa> daftar_mahasiswa;
   int n;
   std::cout<<"Masukkan jumlah mahasiswa yang akan diinputkan: ";std::cin>>n;
-    for (int i = 0; i < n; ++i){
-      Mahasiswa mhs = input_data();
-      daftar_mahasiswa.push_back(mhs);
-    }
+
+  for(int i = 0; i < n; ++i){
+    Mahasiswa mhs = input_data();
+    daftar_mahasiswa.push_back(mhs);
+  }
+
   hitung_golongan_dan_ukt(daftar_mahasiswa);
   tampilkan_data(daftar_mahasiswa);
   return 0;
