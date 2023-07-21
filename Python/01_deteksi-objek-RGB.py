2cd857bdc7bd0bd6df27e4cbf2afe9c4e5d6ba33
Anggiramadyansyah <anggirrr31@gmail.com>
2023-07-21 23:00:41 +0700

    Update 01_deteksi-objek-RGB.py
    Improvisasi.

--- Python/01_deteksi-objek-RGB.py
+++ Python/01_deteksi-objek-RGB.py
@@ -21,61 +21,78 @@
 # SOFTWARE
 
 import cv2
+from tabulate import tabulate
 
 def on_hue_change(value):
     global hue_value
     hue_value = value
 
+def get_color_name(hue, sat, val):
+    if 0 <= hue < 15 or 165 <= hue <= 180:
+        return "Merah"
+    elif 75 <= hue < 105:
+        return "Biru"
+    elif 15 <= hue < 45 or 135 <= hue < 165:
+        return "Orange"
+
 hue_value = 0
 
-cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Tidak dapat membuka kamera!")
+if not cap1.isOpened():
+    print("Kesalahan. Tidak dapat membuka kamera!")
     exit()
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 478)
 
-cv2.namedWindow('Hasil Deteksi Objek')
-cv2.createTrackbar('HUE', 'Hasil Deteksi Objek', hue_value, 180, on_hue_change)
+cv2.namedWindow('Hasil Deteksi Objek 1')
+cv2.createTrackbar('HUE', 'Hasil Deteksi Objek 1', hue_value, 180, on_hue_change)
+
+detected_objects = []
 
 while True:
-    ret, frame = cap.read()
+    ret1, frame1 = cap1.read()
 
-    if not ret:
-        print("Tidak dapat membaca frame dari kamera!")
+    if not ret1:
+        print("Kesalahan. Tidak dapat membaca frame dari kamera!")
         break
+
+    hsv_image1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+
+    lower_color = (hue_value - 10, 50, 50)
+    upper_color = (hue_value + 10, 255, 255)
+
+    mask1 = cv2.inRange(hsv_image1, lower_color, upper_color)
+
+    result1 = cv2.bitwise_and(frame1, frame1, mask=mask1)
+
+    contours1, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    jumlah_objek1 = len(contours1)
+
+    total_area1 = sum([cv2.contourArea(cnt) for cnt in contours1])
+
+    total_area_pixel1 = mask1.shape[0] * mask1.shape[1]
+
+    persentase_area1 = (total_area1 / total_area_pixel1) * 100
+
+    cv2.imshow('Gambar Asli 1', frame1)
+    cv2.imshow('Hasil Deteksi Objek 1', result1)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
-    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    detected_objects.append([result1, hue_value, 50, 50, jumlah_objek1, persentase_area1])
 
-    lower_color = (hue_value - 10, 50, 50)
-    upper_color = (hue_value + 10, 255, 255)
-
-    mask = cv2.inRange(hsv_image, lower_color, upper_color)
-
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    jumlah_objek = len(contours)
-    total_area = sum([cv2.contourArea(cnt) for cnt in contours])
-
-    total_area_pixel = mask.shape[0] * mask.shape[1]
-    persentase_area = (total_area / total_area_pixel) * 100
-
-    # Tampilkan informasi hasil deteksi objek di terminal
-    print("Jumlah Objek yang Terdeteksi:", jumlah_objek)
-    print("Persentase Area Objek:", persentase_area)
-
-    cv2.imshow('Gambar Asli', frame)
-    cv2.imshow('Hasil Deteksi Objek', result)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
+cap1.release()
 cv2.destroyAllWindows()
+
+if len(detected_objects) > 0:
+    print("\nHasil Deteksi Objek:")
+    table_headers = ["No.", "Warna", "Toleransi (H, S, V)", "Objek Terdeteksi", "Area Terdeteksi (%)", "Jarak terdeteksi (m3)"]
+    table_data = [[i+1, get_color_name(data[1], data[2], data[3]), (data[1], data[2], data[3]), data[4], data[5], data[4]*data[5]] for i, data in enumerate(detected_objects)]
+    print(tabulate(table_data, headers=table_headers, tablefmt="grid"))
+else:
+    print("\nKesalahan. Tidak ada hasil deteksi objek!")
