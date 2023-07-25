61a5d8b6453f1a28c58cc87c1da5168cbcf5e8a7
Anggiramadyansyah <anggirrr31@gmail.com>
2023-07-25 18:22:49 +0700

    Update 01_deteksi-objek-RGB.py

--- Python/01_deteksi-objek-RGB.py
+++ Python/01_deteksi-objek-RGB.py
@@ -20,6 +20,28 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2023 Anggiramadyansyah
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+
 import cv2
 from tabulate import tabulate
 
@@ -28,12 +50,14 @@ def on_hue_change(value):
     hue_value = value
 
 def get_color_name(hue, sat, val):
-    if 0 <= hue < 15 or 165 <= hue <= 180:
+    if 0 <= hue < 30 or 150 <= hue <= 179:
         return "Merah"
-    elif 75 <= hue < 105:
+    elif 60 <= hue < 150:
         return "Biru"
-    elif 15 <= hue < 45 or 135 <= hue < 165:
+    elif 30 <= hue < 60:
         return "Orange"
+    elif 315 <= hue < 345:
+        return "Pink"
 
 hue_value = 0
 
@@ -47,7 +71,7 @@ cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
 cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 478)
 
 cv2.namedWindow('Hasil Deteksi Objek 1')
-cv2.createTrackbar('HUE', 'Hasil Deteksi Objek 1', hue_value, 180, on_hue_change)
+cv2.createTrackbar('HUE', 'Hasil Deteksi Objek 1', hue_value, 400, on_hue_change)
 
 detected_objects = []
 
