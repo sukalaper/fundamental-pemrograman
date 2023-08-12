5c87866d5c37362e507fb0bd639791ee5e38507a
Anggiramadyansyah <anggirrr31@gmail.com>
2023-08-12 16:25:44 +0700

    Update logout.php

--- Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/logout.php
+++ Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/logout.php
@@ -1,5 +1,29 @@
 <?php
-session_start();
+session_start(); 
+session_unset();
 session_destroy();
-header('location:../../pages/auth/logout.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+<?php
+echo 
+  "<script>
+    Swal.fire({
+      icon: 'success',
+      title: 'Logout Berhasil',
+      text: 'Anda telah berhasil logout!',
+      showConfirmButton: false,
+      timer: 1800 
+    }).then(function() {
+      window.location.href = '../../pages/auth/login.php'; 
+    });
+  </script>";
+?>
+</body>
+</html>
