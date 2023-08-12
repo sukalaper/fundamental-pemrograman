f9b3591fa10d29ef0284b8fad0acdcaac97e0cf8
Anggiramadyansyah <anggirrr31@gmail.com>
2023-08-12 16:24:29 +0700

    Update cek.php

--- Fullstack/Web-app/Stok-Barang-Sabun-app/kelola_stok/cek.php
+++ Fullstack/Web-app/Stok-Barang-Sabun-app/kelola_stok/cek.php
@@ -1,8 +1,6 @@
 <?php
   if(isset($_SESSION['log'])){
   } else {
-    #echo "Anda belum login, Anda akan dialihkan kembali ke halaman login dalam 3 detik.";
-    #echo "<script>setTimeout(function(){window.location.href='login.php'},5000);</script>";
-    header('location:./../pages/auth/login.php');
+    header('location:../../stok-barang/pages/auth/login.php');
   }
 ?>
