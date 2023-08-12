1880383862cb11505e6c205f813bbdd4e968002e
Anggiramadyansyah <anggirrr31@gmail.com>
2023-08-12 16:25:19 +0700

    Update login.php

--- Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/login.php
+++ Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/login.php
@@ -7,7 +7,7 @@ if(isset($_POST['login'])){
   $checkdatabase = mysqli_query($conn, "SELECT * FROM login where email='$email' and password='$password'");
   $hitung = mysqli_num_rows($checkdatabase);
   if($hitung > 0){
-    $_SESSION['log'] = 'True';
+    $_SESSION['log'] = 'true';
     header('location:../../index.php'); 
   } else {
     header('location:../../pages/peringatan-kesalahan-login.php');  
@@ -22,48 +22,45 @@ if(!isset($_SESSION['log'])){
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Administrator - Login</title>
-    <link href="../../assets/css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-  </head>
-  <body class="bg-primary">
-    <div id="layoutAuthentication">
-      <div id="layoutAuthentication_content">
-        <main>
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-lg-5">
-                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                  <div class="card-header">
-                    <h3 class="text-center font-weight-light my-4">Login</h3>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Administrator - Login</title>
+  <link href="../../assets/css/styles.css" rel="stylesheet">
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+<body class="bg-primary">
+  <div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+      <main class="container">
+        <div class="row justify-content-center align-items-center min-vh-100">
+          <div class="col-md-6">
+            <div class="card shadow-lg border-0 rounded-lg mt-5">
+              <div class="card-header bg-white">
+                <h3 class="text-center font-weight-light my-4">Administrator Login</h3>
+              </div>
+              <div class="card-body">
+                <form method="post">
+                  <div class="mb-3">
+                    <label for="inputEmail" class="form-label">Email address</label>
+                    <input class="form-control" name="email" id="inputEmail" type="email" placeholder="name@example.com" required>
                   </div>
-                  <div class="card-body">
-                    <form method="post">
-                      <div class="form-floating mb-3">
-                        <input class="form-control" name="email" id="inputEmail" type="email" placeholder="name@example.com" />
-                        <label for="inputEmail">Email address</label>
-                      </div>
-                      <div class="form-floating mb-3">
-                        <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Password" />
-                        <label for="inputPassword">Password</label>
-                      </div>
-                      <button class="btn btn-primary" name="login">Login</button>
-                    </form>
+                  <div class="mb-3">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Password" required>
                   </div>
-                </div>
+                  <button class="btn btn-primary w-100" name="login">Login</button>
+                </form>
               </div>
             </div>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="../../assets/js/scripts.js"></script>
-  </body>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="assets/js/scripts.js"></script>
+</body>
 </html>
