05e755e3883cb15da4dc917e66195d49bb1805b3
Anggiramadyansyah <anggirrr31@gmail.com>
2023-08-21 16:16:32 +0700

    Update login.php

--- Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/login.php
+++ Fullstack/Web-app/Stok-Barang-Sabun-app/pages/auth/login.php
@@ -27,7 +27,7 @@ if(!isset($_SESSION['log'])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Administrator - Login</title>
+  <title>Halaman Login</title>
   <link href="../../assets/css/styles.css" rel="stylesheet">
   <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
@@ -39,17 +39,17 @@ if(!isset($_SESSION['log'])){
           <div class="col-md-6">
             <div class="card shadow-lg border-0 rounded-lg mt-5">
               <div class="card-header bg-white">
-                <h3 class="text-center font-weight-light my-4">Administrator Login</h3>
+                <h3 class="text-center font-weight-light my-4">Have an account?</h3>
               </div>
               <div class="card-body">
                 <form method="post">
                   <div class="mb-3">
-                    <label for="inputEmail" class="form-label">Email address</label>
-                    <input class="form-control" name="email" id="inputEmail" type="email" placeholder="name@example.com" required>
+                    <label for="inputEmail" class="form-label">Alamat email</label>
+                    <input class="form-control" name="email" id="inputEmail" type="email" placeholder="sukalaper@space.com" required>
                   </div>
                   <div class="mb-3">
-                    <label for="inputPassword" class="form-label">Password</label>
-                    <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Password" required>
+                    <label for="inputPassword" class="form-label">Kata sandi</label>
+                    <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Kata sandi anda" required>
                   </div>
                   <button class="btn btn-primary w-100" name="login">Login</button>
                 </form>
